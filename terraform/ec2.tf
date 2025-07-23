746762b7134889045ba234e3705e5fd5d66c7fb8
Maor Davidi <maor338@gmail.com>
2025-07-23 23:28:05 +0300

    Fix path to public key and add .gitignore

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("${path.module}/id_rsa.pub")
+  public_key = file("${path.module}keys/id_rsa.pub")
  # ודא שקיים מפתח פומבי במחשב שלך
 }
 
